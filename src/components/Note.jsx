974403b2bea08f9bb7b79d3acee2c0c4021ae4ae
benn-davidd <dbenemu@outlook.com>
2025-04-19 04:53:08 +0100

    code cleanup

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Boruto(2017)</h1>
+      <p>Shinobi no jidai wa owaru</p>
+    </div>
+  );
+}
+
+export default Note;
